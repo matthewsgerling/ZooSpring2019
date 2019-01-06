4599187be36874b27cd284b5416f38ec70474f49
Kelli K <kjkleindorfer@dmacc.edu>
2019-01-06 17:09:48 -0600

    Update AnimalNoise.java

--- src/AnimalNoise.java
+++ src/AnimalNoise.java
@@ -8,12 +8,8 @@ public class AnimalNoise {
 
 	public static void main(String[] args) {
 		Cat fluffy = new Cat();
-		System.out.println(fluffy.speak());
-		
+		System.out.println(fluffy.speak());	
 		Dog max = new Dog("Max", 5, "Golden Lab");
-		System.out.println("My dog's name is " + max.getName());
-		System.out.println("His age is " + max.getAge());
-		System.out.println("He is a " + max.getBreed());
 		System.out.println(max.speak());
 	
    
